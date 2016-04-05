3c0b8149d95d49b4d807d63dd0e7d39f0dd19457
Shubh303 <10023271@sbstudents.org>
2016-04-05 19:33:34 -0400

    Added files via upload

--- row pic/js/scripts.js
+++ row pic/js/scripts.js
@@ -1,5 +1,45 @@
 // Scripts for rows.html
 
 $('.animate-on-mouseenter').on('mouseenter', function() {
-	$(this).toggleClass("animated zoomIn");
+	$(this).toggleClass("animated rollIn");
+});
+
+
+$('.Animate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated zoomInUp");
+});
+
+
+$('.aNimate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated lightSpeedIn");
+});
+
+
+$('.anImate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated zoomInRight");
+});
+
+
+$('.aniMate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated rubberBand");
+});
+
+
+$('.animAte-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated zoomInLeft");
+});
+
+
+$('.animaTe-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated bounceInLeft");
+});
+
+
+$('.animatE-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated zoomInDown");
+});
+
+
+$('.animate-On-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated bounceInRight");
 });
